8beaa277fc082aee9b3a719f8029a2dd859a2dac
Fahad Ibne Hossain <121663028+hridoyfahad30@users.noreply.github.com>
2023-11-23 10:21:43 +0600

    Get All Users service created.

--- src/app/User/user.service.ts
+++ src/app/User/user.service.ts
@@ -1,11 +1,22 @@
 import { TUser } from "./user.interface";
 import { UserModel } from "./user.model";
 
+// Create a User into DB Service
 const createUserIntoDB = async (userInfo: TUser) => {
     const result = await UserModel.create(userInfo);
     return result;
 };
 
+// Get all Users from DB Service
+const getAllUsers = async () => {
+    const result = await UserModel.find();
+    return result;
+}
+
+
+
+// All exported Service
 export const UserService = {
-    createUserIntoDB
+    createUserIntoDB,
+    getAllUsers
 }
